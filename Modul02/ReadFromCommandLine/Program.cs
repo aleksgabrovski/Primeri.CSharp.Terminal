3cb2534b4d00bd8a309d9a50d8ee7f09f4fcbb40
Aleksandar Gabrowski <aleksandar527@gmail.com>
2015-08-27 12:56:25 +0300

    Modul 02,Video 05 - Chetene ot red

---
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReadFromCommandLine
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// Definirane na promenlivi
+			int a = 5, b = 0;
+
+
+			// Vuvejdane na vhodni parametri
+			Console.Write( " Моля въведете а: " );
+			b = Convert.ToInt32 ( Console.ReadLine() );
+
+			//Pechat na rezultata
+			Console.WriteLine ( "Резултата а + б е " + ( a + b ).ToString() + "\n\n\n" );
+		}
+	}
+}
---
+++ Modul02/ReadFromCommandLine/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReadFromCommandLine
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// Definirane na promenlivi
+			int a = 10, b = 0;
+
+			//Vuvejdane na vhodni parametri
+			Console.Write( "Molq vuvedete a: " );
+			b = Convert.ToInt32 (Console.ReadLine() );
+
+			// Pechat na rezlultat
+			Console.WriteLine (a + b);
+        }
+	}
+}
