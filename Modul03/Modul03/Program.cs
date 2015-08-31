09ccb111fa63fbde3accc20b20bf8573bc5b71dd
Aleksandar Gabrowski <aleksandar527@gmail.com>
2015-08-31 17:54:55 +0300

    Modul 03 , Video 03,04 - bool i SQRT stuff

--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -10,9 +10,26 @@ namespace Modul03
 			Console.Write ("> a = "); a = Convert.ToDouble(Console.ReadLine () );
 			Console.Write ("> b = "); b = Convert.ToDouble(Console.ReadLine () );
 
+			// Formatirane s dimensiq
 			string _format = "#0.00 m3";
 
-				Console.WriteLine ("\n\na / b = " + (a / b).ToString( _format ));
+			// Stepenuvane	
+			c = Math.Pow ( a,b );
+			Console.WriteLine ("\n\n>a ^ b = " + c.ToString( _format ));
+
+			// Korenuvane	
+			c = Math.Sqrt(b);
+			Console.WriteLine ("\n\n>b ^ 1/2 = " + c.ToString( _format ));
+
+
+
+			// Zakrugleniq
+			c = Math.Round ( a / b );
+			Console.WriteLine ("\n\n>(a / b) = " + c.ToString() );
+
+
+
+
 		}
 	}
 }
