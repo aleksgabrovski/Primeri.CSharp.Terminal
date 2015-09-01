d404978cb55898e9a5ea859c8cac49c8c3231a9c
Aleksandar Gabrowski <aleksandar527@gmail.com>
2015-09-01 19:04:46 +0300

    Modul 03 , Video 06 - Rabota s uslovni operatori IF

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -15,18 +15,33 @@ namespace BoolExplore
 
 			int _ui = Convert.ToInt32 ( Console.ReadLine () );
 
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 = _ui > 0;
+//			bool _check01 = _ui <= _i.Length;
+//			bool _check02 = _ui > 0;
+//
+//			bool _exp01 = _check01 && _check02; // Logichesko I "&&"
+//			bool _exp02 = _check01 || _check02; // Logichesko ILI "||"
+//			bool _exp03 = !_check01;
+//
+//			Console.WriteLine ("Logichesko I: "  + _exp01.ToString() );
+//			Console.WriteLine ( "Logichesko ILI: " + _exp02.ToString() );
+//			Console.WriteLine ("Otricaniq na " + _check01.ToString () + ": " + _exp03);
 
-			bool _exp01 = _check01 && _check02; // Logichesko I "&&"
-			bool _exp02 = _check01 || _check02; // Logichesko ILI "||"
-			bool _exp03 = !_check01;
+			if (_ui > 0 && _ui <= _i.Length) {
+				Console.WriteLine (_i [_ui - 1]);
+			} else {
+				if (_ui > _i.Length)
+				{
+					Console.WriteLine ("Indeksa e prekaleno golqm\n\n\n");
+				}
 
-			Console.WriteLine ("Logichesko I: "  + _exp01.ToString() );
-			Console.WriteLine ( "Logichesko ILI: " + _exp02.ToString() );
-			Console.WriteLine ("Otricaniq na " + _check01.ToString () + ": " + _exp03);
+				if (_ui <= 0)
+				{
+					Console.WriteLine ("Indeksa e prekaleno maluk\n\n\n");
+				}
+					
+			}
 
-			//Console.WriteLine ( _i [ _ui - 1]);
+
 		}
 	}
 }
